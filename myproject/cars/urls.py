31585785c38e4b2b96ece7e435fe12874ca066d0
Brody Faust <bfwolfgangfaust@gmail.com>
2021-02-14 12:46:01 -0500

    implemented cars and drivers table

--- myproject/cars/urls.py
+++ myproject/cars/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
-
+router = routers.DefaultRouter()
+router.register(r'drivers', views.HeroViewSet)
 
 urlpatterns = [
-    path("<int:pk>/", views.car_detail, name="car_detail"),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest-framework.urls', namespace='rest-framework'))
+    # path("<int:pk>/", views.car_detail, name="car_detail"),
 ]
--- myproject/cars/views.py
+++ myproject/cars/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
+from .serializers import DriverSerializer
 from .models import Car, Driver
 
 # Create your views here.
 
-def car_detail(request, pk):
-    owner_obj = Driver.objects.get(pk=pk)
-    car_objs = Car.objects.filter(owner_id=owner_obj.id)
-    context = {
-        "vehicles": car_objs,
-        "drivers": owner_obj,
-    }
+# Non-RestFramework View
+# def car_detail(request, pk):
+#     owner_obj = Driver.objects.get(pk=pk)
+#     car_objs = Car.objects.filter(owner_id=owner_obj.id)
+#     context = {
+#         "vehicles": car_objs,
+#         "drivers": owner_obj,
+#     }
     
-    return render(request, "car_detail.html", context)
+#     return render(request, "car_detail.html", context)
+
+class HeroViewSet(viewsets.ModelViewSet):
+    queryset = Driver.objects.all().order_by('name')
+    serializer_class = DriverSerializer
